b2c49fd829873d38652adad6a17e2b5335e82345
Logan Ryan <loganryan982@gmail.com>
2025-02-19 10:53:30 +1100

    Implemented delta time

--- MagicPlatformer/MagicPlatformer/main.cpp
+++ MagicPlatformer/MagicPlatformer/main.cpp
@@ -1,32 +1,37 @@
 #include "dewcin.h"
 
-int x = 250, y = 250;
+float x = 250, y = 250;
 
 void GameUpdate(float delta)
 {
-	std::wstring charBuffer = L"delta: " + std::to_wstring(delta) + L"\n";
-	OutputDebugString(charBuffer.c_str());
+	static int frames = 0;
+	static float timePassed = 0.0f;
+
+	frames++;
+	timePassed += delta;
+
+	if (timePassed >= 1.0f)
+	{
+		std::wstring charBuffer = L"delta: " + std::to_wstring(delta) + L"\n";
+		OutputDebugString(charBuffer.c_str());
+
+		timePassed -= 1.0f;
+		frames = 0;
+	}
 
 	dewcin::Renderer::SetPixel(10, 10, { 0, 0, 255 });
+	
+	dewcin::Renderer::FillRectangle({ int(x + 0.5f), int(y + 0.5f), 320, 180 }, { 0, 255, 0 });
 
-	dewcin::Renderer::FillRectangle({ x, y, 320, 180 }, { 0, 255, 0 });
+	if (dewcin::Input::IsKeyPressed(DC_W))
+		y -= 100.0f * delta;
+	else if (dewcin::Input::IsKeyPressed(DC_S))
+		y += 100.0f * delta;
 
-	if (dewcin::Input::IsKeyPressed(DC_UP))
-		y -= 10;
-	else if (dewcin::Input::IsKeyPressed(DC_DOWN))
-		y += 10;
-
-	if (dewcin::Input::IsKeyPressed(DC_LEFT))
-		x -= 10;
-	else if (dewcin::Input::IsKeyPressed(DC_RIGHT))
-		x += 10;
-
-	if (dewcin::Input::IsMouseButtonPressed(DC_MOUSE_MIDDLE))
-		OutputDebugString(L"mouse middle button is pressed!\n");
-
-	dewcin::Input::Position mousePosition = dewcin::Input::getMousePosition();
-	charBuffer = std::to_wstring(mousePosition.x) + L", " + std::to_wstring(mousePosition.y) + L"\n";
-	OutputDebugString(charBuffer.c_str());
+	if (dewcin::Input::IsKeyPressed(DC_A))
+		x -= 100.0f * delta;
+	else if (dewcin::Input::IsKeyPressed(DC_D))
+		x += 100.0f * delta;
 }
 
 dewcin_app_entry_point
